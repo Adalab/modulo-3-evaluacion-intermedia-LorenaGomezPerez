1cd2c0b13420b6bc0855fc1758a30a7516aed945
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-04-26 09:07:16 +0200

    initial html structure and import json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 
 import '../styles/App.scss';
+import originalData from '../data/characters.json';
 
 function App() {
   return (
     <div >
+      <header>
+        <h1>Frases de friends</h1>
+        <nav>
+          <label htmlFor="">Filtar por frases</label>
+          <input type="text" />
+          <label htmlFor="">Filtrar por personajes</label>
+          <input type="" placeholder='Todos'></input>
+        </nav>
+      </header>
+
+      <div>
+        <h2>Añadir una nueva frase</h2>
+        <label htmlFor="">Frase</label>
+        <input type="" /> 
+        <label htmlFor="">Personaje</label> 
+        <input type="text" />
+        <button>Añadir una nueva frase</button>
+      </div>
       
     </div>
   );
