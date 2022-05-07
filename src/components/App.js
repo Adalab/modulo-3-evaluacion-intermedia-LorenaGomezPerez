e46733f27cd351cfd77da448062fde4b32162497
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-05-07 22:02:15 +0200

    correction issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,95 +1,108 @@
-
-import '../styles/App.scss';
-import originalData from '../data/characters.json';
-import { useState } from 'react';
+import "../styles/App.scss";
+import originalData from "../data/characters.json";
+import { useState, useEffect } from "react";
+import getDataQuotes from "../services/fetch";
 
 function App() {
-
   //Variables de estado
+  //fetch, listado de frases completas, añadir frase, filtrar por frase, filtrar por personaje
 
+  const [DataQuotes, setDataQuotes] = useState([]);
   const [data, setdata] = useState(originalData);
-  const [newList, setNewList] = useState("");
+  const [newList, setNewList] = useState({
+    quote: "",
+    character: "",
+  });
   const [dataFilter, setDataFilter] = useState("");
-  const [dataCharacter, setDataCharacter] = useState("");
+  const [dataCharacter, setDataCharacter] = useState("All");
+
+  // Fetch
+
+  useEffect(() => {
+    if (DataQuotes.length === 0) {
+      getDataQuotes().then((datafromAPI) => {
+        setDataQuotes(datafromAPI);
+      });
+    }
+  }, []);
 
   //Hago map para recorrer el array y lo pinte
 
   const html = data
-  .filter((quoteFilter)=>
-  quoteFilter.quote.toLowerCase().includes(dataFilter.toLowerCase()))
-  .filter((characterFilter)=>
-  characterFilter.character.includes(dataCharacter))
-  .map((dataCharacter, index) =>
-  <li key={index}>
-    <p>{dataCharacter.quote}</p>
-    <p>{dataCharacter.character}</p>
-    </li>
-  );
-
+    .filter((quoteFilter) =>
+      quoteFilter.quote.toLowerCase().includes(dataFilter.toLowerCase())
+    )
+    .filter((characterFilter) => {
+      if (dataCharacter === "All") {
+        return true;
+      } else if (dataCharacter === characterFilter.character) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    // .filter((characterFilter) =>
+    //   characterFilter.character.includes(dataCharacter)
+    // )
+    .map((dataCharacter, index) => (
+      <li key={index}>
+        <p>{dataCharacter.quote}</p>
+        <p>{dataCharacter.character}</p>
+      </li>
+    ));
 
   //Función manejadora para añadir nueva frase
 
   const handleNewList = (ev) => {
-    setNewList(ev.target.value);
-    
-  }
+    setNewList({
+      ...newList,
+      [ev.target.name]: ev.target.value,
+    });
+  };
 
   //Función manejadora del botón
 
   const handleClickButton = () => {
-
-    const newPhrase = {
-      quote: newList,
-      character: newList
-    }
-    const newListData = [...data,newPhrase]
-    setdata(newListData);
+    setdata([...data, newList]);
     setNewList({
-      quote: '' ,
-      character: ''
-      
+      quote: "",
+      character: "",
     });
-}
+  };
 
-//función para controlar el formulario
+  //función para controlar el formulario
 
-const handleForm = (ev) => {
-  ev.preventDefault();
-}
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  };
 
-//función manejadora para controlar el filtrado por frase
+  //función manejadora para controlar el filtrado por frase
 
-const handleFilterData = (ev) => {
+  const handleFilterData = (ev) => {
+    setDataFilter(ev.target.value);
+  };
 
-  setDataFilter(ev.target.value);
+  //función manejadora para controlar el filtrado por personaje
 
-}
+  const handleFilterCharacter = (ev) => {
+    setDataCharacter(ev.target.value);
+  };
 
-//función manejadora para controlar el filtrado por personaje
-
-const handleFilterCharacter = (ev) => {
-
-  setDataCharacter(ev.target.value);
-
-}
-
-
-
-  
   return (
-    <div >
+    <div>
       <header>
         <h1>Frases de friends</h1>
         <form onSubmit={handleForm}>
           <label htmlFor="listPhrase">Filtrar por frases</label>
-          <input type="text" value={dataFilter} onChange={handleFilterData}/>
+          <input type="text" value={dataFilter} onChange={handleFilterData} />
           <label htmlFor="list">Filtrar por personajes</label>
           <select
             name="list"
             id="list"
             value={dataCharacter}
-            onChange={handleFilterCharacter}>
-            <option>Todos</option>
+            onChange={handleFilterCharacter}
+          >
+            <option value="All">Todos</option>
             <option>Ross</option>
             <option>Phoebe</option>
             <option>Joey</option>
@@ -100,24 +113,29 @@ const handleFilterCharacter = (ev) => {
         </form>
       </header>
       <main>
-        <ul>
-          {html}
-        </ul> 
+        <ul>{html}</ul>
       </main>
 
-      <div>
+      <form onSubmit={handleForm}>
         <h2>Añadir una nueva frase</h2>
-        <label htmlFor="phrase">Frase</label>
-        <input type="text" name="phrase" id="phrase" value={newList.quote} onChange={handleNewList}/> 
-        <label htmlFor="friends">Personaje</label> 
-        <input type="text" name="friends" id="friends" value={newList.character} onChange={handleNewList}/>
+        <label htmlFor="quote">Frase</label>
+        <input
+          type="text"
+          name="quote"
+          value={newList.quote}
+          onChange={handleNewList}
+        />
+        <label htmlFor="character">Personaje</label>
+        <input
+          type="text"
+          name="character"
+          value={newList.character}
+          onChange={handleNewList}
+        />
         <button onClick={handleClickButton}>Añadir una nueva frase</button>
-      </div>
-      
+      </form>
     </div>
-  
   );
-  
 }
 
 export default App;
